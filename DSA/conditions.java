4573369c59ea6d639731c9cc14571b0307725559
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2023-11-27 02:13:57 +0530

    dsa material day-2

---
+++ DSA/conditions.java
@@ -0,0 +1,25 @@
+public class conditions {
+    public static void main(String[] args) {
+
+        // conditions statements {if, else,switch case }
+
+
+         String ex = "2000";
+
+         String dholu = "1000"; // 4 - int , double, float, Long
+
+        if (true) {
+            System.out.println("Not Bhikharan");
+            
+            
+        }else if (true) {
+            System.out.println("Bhikharan");
+        }
+        
+        else{
+            System.out.println("7 carod");
+        }
+
+        
+    }
+}
--- DSA/hello.java
+++ DSA/hello.java
@@ -27,14 +27,5 @@ public class hello {
         // codervivek@ybl
 
 
-
-       
-
-
-
-
-
-       
-
     }
 }
