1ab27caa67e339b15f0ae9446bd80ffa8fe3fcb9
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2023-12-06 19:48:18 +0530

    enhance like button

--- JS/InstaLikeButton/script.js
+++ JS/InstaLikeButton/script.js
@@ -1,11 +1,25 @@
-var poost = document.querySelector(".container")
-var heart = document.querySelector("i")
+var poost = document.querySelector(".container");
+var heart = document.querySelector("i");
+var togel = 0
 
-poost.addEventListener("dblclick" , function(){
+poost.addEventListener("dblclick", function () {
+  console.log("click is worlking");
 
-  console.log("click is worlking")
+  heart.style.color = "red";
+  heart.className = "ri-heart-fill";
+});
 
-  heart.style.color = "red"
-  heart.className = "ri-heart-fill"
+heart.addEventListener("click", function () {
 
-})
+  if (togel == 0) {
+    heart.style.color = "red";
+    heart.className = "ri-heart-fill";
+    togel = 1
+  } else {
+    heart.className = "ri-heart-line";
+    heart.style.color = "black";
+    togel = 0
+    
+  }
+  
+});
