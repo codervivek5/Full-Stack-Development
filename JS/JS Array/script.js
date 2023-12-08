76e1e9489d6f3d35426687c5a2bbff11a6b298c4
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2023-12-08 20:01:13 +0530

    array in js

---
+++ JS/JS Array/script.js
@@ -0,0 +1,42 @@
+
+
+
+// push -  add element at the end of array
+// pop - remove element at the end of array
+
+// unshift - add element at the begining of array
+// shift - remove element at the begining of array
+// iteration - 
+// delete
+// slice
+// toString
+
+
+var ureee = [70,80,85,17,12,99];
+
+ureee[0] = "Ramu"
+ureee[2] = "dahi bada"
+
+
+
+// ureee.forEach(elem => {
+
+//     document.write(elem , ", ")
+    
+// });
+
+// for(let i = 0 ;  i < ureee.length ; i++){
+
+//     document.write(ureee[i], " ")
+
+// }
+
+// Array  - Class
+
+
+// you need to use any class or there element or method or function or anything else 
+// firstly you need to create an object of that perticular class then use that via object name 
+// var ureee = new Array(2,3,45,6);
+
+
+
